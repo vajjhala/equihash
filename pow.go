7ecbf6d73e672cc49845ee857cee5e49ca8b8ec5
Vajjhala <v2.surya@gmail.com>
2018-04-27 10:51:28 -0400

    First commit

---
+++ pow.go
@@ -0,0 +1,21 @@
+package equicash
+
+import (
+	"fmt"
+	"crypto/sha256"
+	"strconv"
+)
+
+//var DEBUG bool
+
+//func generalised_birthday(d, n, k int) []int {
+//	collisoin_length := n/(k+1)
+//	if DEBUG {
+//
+//	}
+//	return
+//}
+
+func main() {
+	fmt.Printf(strconv.Itoa(sha256.BlockSize))
+}
